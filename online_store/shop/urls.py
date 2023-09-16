084bf55ea5e66b06d1b6d8c4b99ba948d9657a63
yar.koniukhov <yar.koniukhov@gmail.com>
2023-09-16 17:02:37 +0300

    corrected urls shop/urls

--- online_store/shop/urls.py
+++ online_store/shop/urls.py
@@ -5,14 +5,9 @@ app_name = 'shop'
 
 
 urlpatterns = [
+
     path('', views.index, name='index'),
+    path('<str:category_name>/', views.product_list, name='face_list'),
 
-    path('product/', views.product_list, name='product_list'),
-    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('brand/<slug:brand_slug>/', views.product_list, name='product_list_by_brand'),
-
-    path('product/size/<slug:size_slug>/', views.product_list, name='product_list_by_size'),
-
-    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-
+    path('<int:id>/<slug:url>/', views.product_detail, name='product_detail'),
 ]
