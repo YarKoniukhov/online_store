1c79c333911799499374cde47205091161936945
yar.koniukhov <yar.koniukhov@gmail.com>
2024-01-24 18:43:18 +0200

    corrected

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['Yar.pythonanywhere.com', 'mysite.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
